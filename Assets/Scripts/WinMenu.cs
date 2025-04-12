acea0859fa4557e6baa84dbe164014667d124741
Aiden Thacker <aidenthacker86@gmail.com>
2025-04-11 20:47:09 -0500

    Made the Menus and there functionality

---
+++ Assets/Scripts/WinMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class WinMenu : MonoBehaviour
+{
+    public bool didWin = false;
+
+    public GameObject winSection;
+    public PauseMenu pauseMenu;
+    public void Win()
+    {
+        didWin = true;
+        pauseMenu.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        winSection.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1.0f;
+        pauseMenu.enabled = true;
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1.0f;
+        pauseMenu.enabled = true;
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void QuitGame()
+    {
+        #if(UNITY_EDITOR)
+        Debug.Log("Quiting Play Mode");
+        EditorApplication.ExitPlaymode();
+        #else
+        Debug.Log("Quitting Build");
+        Application.Quit();
+        #endif
+    }
+}
