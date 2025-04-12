0831d3fae7742bae84b1649168a068c53435a42f
brilannebays <brilannebays@gmail.com>
2025-04-11 21:13:18 -0500

    Handled some player interactions - incomplete for now

---
+++ Assets/Scripts/NPCInteractions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NPCInteractions : MonoBehaviour {
+    public GameObject dialogPanel;
+    public TextMeshProUGUI firstNPCdialog;
+    public string[] NPCWords;
+    private int index;
+
+    public float wordSpeed;
+    //public bool 
+}
