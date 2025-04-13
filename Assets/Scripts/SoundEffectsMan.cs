c9e40e2c89f2e79228e8fb614caa3e53ecbe1072
brilannebays <brilannebays@gmail.com>
2025-04-13 13:43:17 -0500

    this should be fixed

--- Assets/Scripts/SoundEffectsMan.cs
+++ Assets/Scripts/SoundEffectsMan.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundEffectsMan : MonoBehaviour
 {
     Scene theSceneImIn;
 
     public AudioSource audioSource;
-    public AudioClip[] = audioClips;
+    public AudioClip[] audioClips;
+    Scene currentScene;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,9 @@ public class SoundEffectsMan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Scene currentScene = SceneManager.GetActiveScene();
+        currentScene = SceneManager.GetActiveScene();
     }
-/*
+
     public void schoolSceneSd() {
         while (currentScene == theSceneImIn) {
 
@@ -30,10 +32,11 @@ public class SoundEffectsMan : MonoBehaviour
         yield return new WaitForSeconds(10f);
         while (currentScene == theSceneImIn) {
             foreach (AudioClip clip in audioClips) {
-
+                audioSource.clip = clip;
+                audioSource.Play();
+                yield return new WaitForSeconds(10f);
             }
         }
     }
 }
-*/
-}
+
